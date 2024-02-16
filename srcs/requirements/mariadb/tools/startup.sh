04ce1b4122b29e34371a899ef8c82d98cd35611c
LilNex <nextorchq@gmail.com>
2024-02-16 23:23:47 +0000

    Sync

---
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -0,0 +1,15 @@
+
+service mariadb start
+# sleep 10
+
+# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';",
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# mysql -e "FLUSH PRIVILEGES;"
+sleep 50000
+
+# exec mysqld_safe
+
+
+# GRANT ALL PRIVILEGES ON master.* to 'root'@'%' IDENTIFIED BY 'password';
