b3e52e4736f81973dc09585b1864fb79aa1516cf
LilNex <nextorchq@gmail.com>
2024-02-17 19:53:28 +0000

    Mandatory part done

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -5,10 +5,10 @@ if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
     mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
     mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-    mysql -u root -e "FLUSH PRIVILEGES;" -p "${SQL_ROOT_PASSWORD}"
+    mysql -u root -e "FLUSH PRIVILEGES;" 
 fi
 
-mysqladmin shutdown -p ${SQL_ROOT_PASSWORD}
+mysqladmin shutdown 
 exec mysqld --user=mysql
+sleep 3000
 
--- srcs/requirements/wordpress/tools/auto_conf.sh
+++ srcs/requirements/wordpress/tools/auto_conf.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-wp core download --path=/var/www/html/wordpress --allow-root
+mkdir -p /run/php
+mkdir -p /var/www/wordpress
+wp core download --path=/var/www/wordpress --allow-root
+
+cd /var/www/wordpress
+
+wp config create	--allow-root \
+    --dbname=$SQL_DATABASE \
+    --dbuser=$SQL_USER \
+    --dbpass=$SQL_PASSWORD \
+    --dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+rm -f wp-config-sample.php
+
+wp core install --url=ismailchaiq.com --title=Example --path='/var/www/wordpress' --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --allow-root
+
+php-fpm7.4 -F -R
