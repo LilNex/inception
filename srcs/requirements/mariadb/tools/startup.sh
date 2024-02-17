75411e92bda8f6c289640b151356e4a98380f323
LilNex <nextorchq@gmail.com>
2024-02-17 00:07:37 +0000

    Creating user and privileges on mariadb for wordpress

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -1,15 +1,14 @@
 
 service mariadb start
-# sleep 10
 
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';",
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-# mysql -e "FLUSH PRIVILEGES;"
-sleep 50000
+if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+    mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+    mysql -u root -e "FLUSH PRIVILEGES;" -p "${SQL_ROOT_PASSWORD}"
+fi
 
-# exec mysqld_safe
+mysqladmin shutdown -p ${SQL_ROOT_PASSWORD}
+exec mysqld --user=mysql
 
-
-# GRANT ALL PRIVILEGES ON master.* to 'root'@'%' IDENTIFIED BY 'password';
